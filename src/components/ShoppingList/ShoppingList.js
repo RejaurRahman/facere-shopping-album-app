24a056f18649de36e3c3d59a38aaddba7a1e1625
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2021-10-28 10:51:18 +0100

    shopping item list is now dynamic using map function

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useState } from 'react';
 import ShoppingFilter from '../ShoppingFilter/ShoppingFilter';
 import ShoppingItem from '../ShoppingItem/ShoppingItem';
 import Card from '../UI/Card';
@@ -16,26 +15,15 @@ const ShoppingList = (props) => {
   return (
     <Card className="list">
       <ShoppingFilter selected={filteredCategory} onChangeFilter={filterChangeHandler} />
-      <ShoppingItem 
-        title={props.items[0].title} 
-        amount={props.items[0].amount} 
-        category={props.items[0].category}
-      />
-      <ShoppingItem 
-        title={props.items[1].title} 
-        amount={props.items[1].amount} 
-        category={props.items[1].category}
-      />
-      <ShoppingItem 
-        title={props.items[2].title} 
-        amount={props.items[2].amount} 
-        category={props.items[2].category} 
-      />
-      <ShoppingItem 
-        title={props.items[3].title} 
-        amount={props.items[3].amount} 
-        category={props.items[3].category} 
-      />
+      {
+        props.items.map((expense) => (
+          <ShoppingItem 
+            title={expense.title}
+            amount={expense.amount}
+            category={expense.category}
+          />
+        ))
+      }
     </Card>
   );
 };
