27b61fe8a2e32ac8c61ed94dcb269019c46c1612
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2021-10-28 11:51:14 +0100

    tidy up of shopping item component

--- src/components/ShoppingItem/ShoppingItem.js
+++ src/components/ShoppingItem/ShoppingItem.js
@@ -1,29 +1,16 @@
-
-   
-import React, { useState } from 'react';
+import React from 'react';
 import Card from '../UI/Card';
 
 import './ShoppingItem.css';
 
 const ShoppingItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [category, setCategory] = useState(props.category);
-
-  const clickTitleHandler = () => {
-    setTitle('Updated!');
-  };
-
-  const clickCategoryHandler = () => {
-    setCategory('Updated Category!');
-  };
-
   return (
-    <Card className="list">
-      <div>{category}</div>
-      <div>{title}</div>
-      <button onClick={clickTitleHandler}>change title</button> 
-      <button onClick={clickCategoryHandler}>change category</button> 
-    </Card>
+    <li>
+      <Card className="shoppingItem">
+        <div>{props.category}</div>
+        <div>{props.title}</div> 
+      </Card>
+    </li>
   );
 };
 
