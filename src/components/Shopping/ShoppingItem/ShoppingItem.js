1135c98a32f57952e64a19d7945f7da3062dbf66
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2021-11-14 14:15:17 +0000

    svg icons added for edit and delete icons

--- src/components/Shopping/ShoppingItem/ShoppingItem.js
+++ src/components/Shopping/ShoppingItem/ShoppingItem.js
@@ -4,6 +4,9 @@ import Card from '../../UI/Card/Card';
 
 import './ShoppingItem.css';
 
+import { ReactComponent as EditIcon } from '../../../assets/icons/edit-pencil.svg';
+import { ReactComponent as DeleteIcon } from '../../../assets/icons/trash-bin.svg';
+
 const ShoppingItem = (props) => {
   const deleteHandler = (event, id) => {
     props.onDeleteShopping(id);
@@ -17,8 +20,19 @@ const ShoppingItem = (props) => {
           <p className="item__name">{props.title}</p>
         </div>
         <div className="item__right">
-          <Button type="button" className="item__button item__button--edit">Edit</Button>
-          <Button type="button" className="item__button item__button--delete" onClick={(event) => deleteHandler(event, props.shopItemId)}>Delete</Button>
+          <Button 
+            type="button" 
+            className="item__button item__button--edit"
+          >
+            <EditIcon />
+          </Button>
+          <Button 
+            type="button" 
+            className="item__button item__button--delete" 
+            onClick={(event) => deleteHandler(event, props.shopItemId)}
+          >
+            <DeleteIcon />
+          </Button>
         </div>
       </Card>
     </li>
