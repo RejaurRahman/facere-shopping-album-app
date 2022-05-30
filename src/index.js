8dbf1f3e511f9c8d7452ecffe87244acaf19f5fe
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-05-31 00:58:13 +0100

    sentry added to track and monitor error logging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+Sentry.init({
+  dsn: "https://9de6de81795a4ee38479b43a279b49e2@o1170466.ingest.sentry.io/6454738",
+  integrations: [new Integrations.BrowserTracing()],
+  autoSessionTracking: true,
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
