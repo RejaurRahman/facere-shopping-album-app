e6d9c45c09df5c42d3ca966150e8a4edfadd88d7
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-09-28 03:34:18 +0100

    added portal for modal

--- src/components/Form/EditShoppingForm/EditShoppingForm.js
+++ src/components/Form/EditShoppingForm/EditShoppingForm.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import Button from "../../UI/Button/Button";
 import Modal from "../../UI/Modal/Modal";
-
 import { ReactComponent as TickIcon } from "../../../assets/icons/tick.svg";
 import { ReactComponent as MinusIcon } from "../../../assets/icons/minus-cancel.svg";
-
 import "./EditShoppingForm.scss";
 
 const EditShoppingForm = (props) => {
@@ -41,46 +40,51 @@ const EditShoppingForm = (props) => {
   };
 
   return (
-    <Modal onClick={props.onCancelEdit}>
-      <form
-        onSubmit={submitHandler}
-        className="form form__edit"
-      >
-        <input
-          type="text"
-          value={enteredTitle}
-          className="input input__edit-text"
-          name="text"
-          placeholder="Change entered task"
-          onChange={titleChangeHandler}
-        />
-        <input
-          type="text"
-          value={enteredCategory}
-          className="input input__edit-category"
-          name="text"
-          placeholder="Change entered Category (For Ex. Food)"
-          onChange={categoryChangeHandler}
-        />
-        <div className="modal__footer">
-          {validationError && (
-            <p className="error__message">{validationError}</p>
-          )}
-          <div className="modal__footer--container">
-            <Button type="submit" className="button__update">
-              <TickIcon />
-            </Button>
-            <Button
-              type="button"
-              className="button__exit"
-              onClick={props.onCancelEdit}
-            >
-              <MinusIcon />
-            </Button>
-          </div>
-        </div>
-      </form>
-    </Modal>
+    <>
+      {ReactDOM.createPortal(
+        <Modal onClick={props.onCancelEdit}>
+          <form
+            onSubmit={submitHandler}
+            className="form form__edit"
+          >
+            <input
+              type="text"
+              value={enteredTitle}
+              className="input input__edit-text"
+              name="text"
+              placeholder="Change entered task"
+              onChange={titleChangeHandler}
+            />
+            <input
+              type="text"
+              value={enteredCategory}
+              className="input input__edit-category"
+              name="text"
+              placeholder="Change entered Category (For Ex. Food)"
+              onChange={categoryChangeHandler}
+            />
+            <div className="modal__footer">
+              {validationError && (
+                <p className="error__message">{validationError}</p>
+              )}
+              <div className="modal__footer--container">
+                <Button type="submit" className="button__update">
+                  <TickIcon />
+                </Button>
+                <Button
+                  type="button"
+                  className="button__exit"
+                  onClick={props.onCancelEdit}
+                >
+                  <MinusIcon />
+                </Button>
+              </div>
+            </div>
+          </form>
+        </Modal>,
+        document.getElementById("modal-root")
+      )}
+    </>
   );
 }
 
