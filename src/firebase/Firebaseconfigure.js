523e23ba105493515c1cbc384b1d4050b778dbd8
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-12-02 23:51:22 +0000

    add environment variables from firebase datastore

--- src/firebase/Firebaseconfigure.js
+++ src/firebase/Firebaseconfigure.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCIl-WYPPRdUYbRY-iTj8io1SSTZ3Ca750',
-  authDomain: 'facere-shopping-album.firebaseapp.com',
-  databaseURL: 'https://facere-shopping-album-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'facere-shopping-album',
-  storageBucket: 'facere-shopping-album.appspot.com',
-  messagingSenderId: '64508986565',
-  appId: '1:64508986565:web:74ce57a420a444697f5cac'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL:process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
